550263d31a24d4d9bb44f4a16f2822e34447f4f6
nsy5687 <quddls5687@naver.com>
2021-05-21 02:19:39 +0900

    그리디

--- 그리디_숫자카드게임.py
+++ 그리디_숫자카드게임.py
@@ -5,9 +5,9 @@ result=0
 for i in range(n):
     data = list(map(int, input().split()))
 
-    min_value = min(data)
+    min_value = min(data)#min(리스트) : 리스트 전체의 대상으로 각 항목마다 최소 값을 구한다
 
-    result=max(result,min_value)
+    result=max(result,min_value)#max(이전 비교할 숫자, 현재 비교할 숫자)
 
 print(result)
 
@@ -20,7 +20,7 @@ for i in range(n):
 
     min_value = 10000
     for j in data:
-        min_value = min(min_value,j)
+        min_value = min(min_value,j)#min(이전 비교할 숫자, 현재 비교할 숫자)
     
     result=max(result,min_value)
 
